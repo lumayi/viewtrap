1e1947d24b6a1342f146b7a6166a2688a31d183b
greeneeya <suzythetravelerr@gmail.com>
2023-07-11 09:45:35 +0900

    NotFound 추가

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Videos from './pages/Videos';
+import NotFound from './pages/NotFound';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <NotFound />,
     children: [
       {
         index: true,
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export default function NotFound() {
-  return <div>Sorry, we could not find the page</div>;
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="flex flex-col justify-center items-center gap-4">
+        <iframe
+          src="https://giphy.com/embed/7NoNw4pMNTvgc"
+          width="480"
+          height="480"
+          class="giphy-embed"
+          allowFullScreen
+          title="notfound-image"
+          className="rounded-full"
+        ></iframe>
+        <h2 className="text-xl font-semibold">
+          요청하신 페이지를 찾을 수 없습니다 😿
+        </h2>
+      </div>
+    </div>
+  );
 }
